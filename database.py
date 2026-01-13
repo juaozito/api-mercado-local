37a7bb994b118190562b4854546d48c1caffe218
Jota <reboucasjoao85@gmail.com>
2026-01-13 10:39:31 -0400

    Fix: removendo importações relativas em todos os arquivos

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 # IMPORTANTE: Importamos as configurações que você criou
-from .config import settings
+from config import settings
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
